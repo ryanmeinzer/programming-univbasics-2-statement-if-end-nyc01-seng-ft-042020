2ec9197fc0c97bd6996a220cea583c0245dfc440
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-03-31 21:18:08 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,9 +1,9 @@
 # Write your solution here
 
-puts Time.now.to_i
+# puts Time.now.to_i
 
-# if Time.now % 2 == 0
-#   puts "Even!"
-# else
-#   puts "Odd!"
-# end
+if Time.now.to_i % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
